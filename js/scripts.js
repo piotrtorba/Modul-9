69f2ffec849da5d89014648218630c6ec1a82af9
Torba <torba.piotr@gmail.com>
2017-05-09 22:11:47 +0200

    m

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var men = ['Paul', 'Bob', 'Robert', 'Henry'];
+var woman = ['Melisa', 'Amanda', 'Julia', 'Kate'];
+var allNames = men.concat(woman);
+var newName = 'Bart';
+if (allNames.indexOf(newName) === -1) {
+		allNames.push(newName);
+} else { 
+	console.log('error');
+}
+conosle.log(allNames);
