d472e73dc6b54a516a5fdfb572dbfc7052c85694
Torba <torba.piotr@gmail.com>
2017-05-16 22:17:51 +0200

    m

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-drawTree(7) {
-	for (var i=0 ; i<7 ; i++) {
-		var star = ();
-		/*Hej Michal, wydaje mi sie ,ze do tego momentu kod jest OK.
-		Cos chyba namieszalem w parametrach drugiej petli 'for'. Zerknij jak mozesz
-		bo nie kumam bledu. Pozdr.*/
-		for (var j=i++ ; j==7 ; i++) {
-			var star = star += '*';
-		}
-	}
-	console.log(star);
+var przycisk = document.getElementsByClassName('button');
+for (var k = 0; k < przycisk.length; k++) {
+ 	alert (przycisk[k].innerText);
 }
+console.log(przycisk);
