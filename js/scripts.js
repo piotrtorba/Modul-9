96872dc4c6932122f4f2639928fb5ed1426aa3db
Torba <torba.piotr@gmail.com>
2017-05-21 13:17:42 +0200

    m

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
-drawTree(7) {
-	for (var i=0 ; i<7 ; i++) {
-		var star = ();
-		/*Hej Michal, wydaje mi sie ,ze do tego momentu kod jest OK.
-		Cos chyba namieszalem w parametrach drugiej petli 'for'. Zerknij jak mozesz
-		bo nie kumam bledu. Pozdr.*/
-		for (var j=i++ ; j==7 ; i++) {
-			var star = star += '*';
-		}
-	}
-	console.log(star);
-}
+var list = document.getElementById("list");
+var add = document.getElementById("addElem");
+var li_element = document.getElementsByTagName("li");
+add.addEventListener("click", function() {
+	var element = document.createElement("li");
+	element.innerHTML = "item "+ li_element.length;
+	list.appendChild(element);
+});
